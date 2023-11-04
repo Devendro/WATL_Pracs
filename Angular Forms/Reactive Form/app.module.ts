165c19e3f3f889b7eb34561390165fc2a0ea2b42
Devendro <76817042+Devendro@users.noreply.github.com>
2023-11-04 07:43:49 +0530

    Practical 10

--- Angular Forms/Reactive Form/app.module.ts
+++ Angular Forms/Reactive Form/app.module.ts
@@ -5,13 +5,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
+
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    AboutComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
